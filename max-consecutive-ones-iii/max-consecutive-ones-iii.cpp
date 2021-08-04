395edbea602621bd5e7920b664ee2cb8b43be4d1
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2021-08-04 14:46:30 +0530

    Time: 52 ms (74.67%), Space: 55.5 MB (32.88%) - LeetHub

--- max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -5,7 +5,10 @@ public:
         int ans = 0;
         for (j = 0; j < A.size(); ++j) {
             if (A[j] == 0) K--;
-            if (K < 0 && A[i++] == 0) K++;
+            while (K < 0){
+                if(A[i]==0) K++;
+                i++;
+            }
             ans = max(ans,j-i+1);
         }
         return ans;
