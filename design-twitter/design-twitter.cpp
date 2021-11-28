27b65d9e8075dbc923b8f1df2a75327db889db2e
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2021-11-28 14:47:49 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (42.63%) - LeetHub

--- design-twitter/design-twitter.cpp
+++ design-twitter/design-twitter.cpp
@@ -1,20 +1,36 @@
 class Twitter {
-public:
+private:
     int timeOfTweet = 0;
     map<int,set<pair<int,int>,greater<pair<int,int>>>> posts; //decreasing order
     map<int,set<int>> followedUers;
     int maxTweets = 10;
 
+    set<int> getAllFollowers(int userId){
+        return followedUers[userId];
+    }
+
+    set<pair<int,int>,greater<pair<int,int>>> getAllLatestTweets(int userId){
+        set<pair<int,int>,greater<pair<int,int>>> allLatestTweets;
+        for(auto tweetsOfUser:posts[userId]) allLatestTweets.insert({tweetsOfUser.first,tweetsOfUser.second});
+
+        // & dont make copies
+        for(const auto& followerId:getAllFollowers(userId)){
+            int noOfTweets = maxTweets;
+            for(const auto& [time,tweetId]:posts[followerId]){
+                if(noOfTweets<=0) break;
+                allLatestTweets.insert({time,tweetId});
+                noOfTweets--;
+            }
+        }
+        return allLatestTweets;
+    }
+
+public:
     /** Initialize your data structure here. */
     Twitter(){
         timeOfTweet = 0;
         posts.clear();
         followedUers.clear();
-    }
-
-
-    set<int> getAllFollowers(int userId){
-        return followedUers[userId];
     }
 
     /** Compose a new tweet. */
@@ -25,29 +41,16 @@ public:
     
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     vector<int> getNewsFeed(int userId){
-        set<pair<int,int>,greater<pair<int,int>>> allLatestTweets;
-        for(auto i:posts[userId]) allLatestTweets.insert({i.first,i.second});
-
-        for(auto i:getAllFollowers(userId)){
-            int followerId = i;
-            int noOfTweets = maxTweets;
-            for(auto tweets:posts[followerId]){
-                if(noOfTweets<=0) break;
-                allLatestTweets.insert({tweets.first,tweets.second});
-                noOfTweets--;
-            }
-        }
+        set<pair<int,int>,greater<pair<int,int>>> allLatestTweets = getAllLatestTweets(userId);
 
         int noOfTweets = maxTweets;
         vector<int> latestTweets;
-        for(auto allTweets:allLatestTweets){
+        for(const auto& allTweets:allLatestTweets){
             if(noOfTweets<=0) break;
             latestTweets.push_back(allTweets.second);
             noOfTweets--;
         }
-
         return latestTweets;
-
     }
     
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
