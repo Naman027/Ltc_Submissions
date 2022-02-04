94aa8b0a9ee7ab53826b69351dcf5688f31c7aac
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2022-02-04 20:54:05 +0530

    Time: 212 ms (32.57%), Space: 84.5 MB (10.62%) - LeetHub

---
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long long double
+
+#define endl '\n'
+#define fo(i,m,n) for(int i=m;i<n;i++)
+#define foe(i,m,n) for(int i=m;i<=n;i++)
+#define mod 1000000007
+#define mod2 998244353                                                                       
+#define F first
+#define S second
+#define pb push_back
+#define MP make_pair
+#define vt vector
+#define sz(x) (int)x.size()
+#define all(v) v.begin(),v.end()
+#define deb(x) cerr<<#x<<'='<<(x)<<endl;
+#define debp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define deb2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debv(v) {cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;}
+#define maxheap priority_queue<ll>
+#define minheap priority_queue<ll,vector<ll>,greater<ll>>
+const ll INF=1e18L;
+const double PI=4*atan(1);
+const double eps=1e-8; 
+int dx[]={-1,0,1,0,-1,1,1,-1};
+int dy[]={0,1,0,-1,1,1,-1,-1};
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+        int c = 0;
+        map<int,int> mp;
+        mp[0] = -1;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==1) c++;
+            else c--;
+            if(mp.find(c)!=mp.end()){
+                ans = max(ans,i-mp[c]);
+            }
+            else{
+                mp[c] = i;
+            }
+        }
+
+        return ans;
+
+    }
+};
