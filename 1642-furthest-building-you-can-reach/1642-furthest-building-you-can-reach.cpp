e9fa87bfc068343b3b4390a3bb85e17ae9134be6
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2022-06-21 22:25:31 +0530

    Time: 177 ms (35.04%), Space: 53.9 MB (34.72%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-
-    bool check(int mid,vector<int>& heights,int bricks,int ladders){
-        priority_queue<int> pq;
-        for(int i=1;i<=mid;i++){
-            if(heights[i]>heights[i-1])
-                pq.push(heights[i]-heights[i-1]);
-        }
-        long long int cur = 0;
-        while(!pq.empty()){
-            if(ladders==0){
-                cur+=(long long int)pq.top();
-            }
-            else ladders--;
-            pq.pop();
-        }
-        return (cur<=bricks);
-    }
-
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n = heights.size();
-        int l = 0, r = n-1;
-        while(r-l>1){
-            int mid = (l+r)/2;
-            if(check(mid,heights,bricks,ladders)){
-                l = mid;
-            }
-            else r = mid-1;
-        }      
+        priority_queue<int> maxB;
+    
+        int i=0, diff =0; 
+        for(i=0; i<heights.size()-1; i++){ 
+            diff = heights[i+1]-heights[i];
 
-        if(check(r,heights,bricks,ladders)) return r;
-        return l;
+            if(diff <= 0){
+                continue;
+            }
+
+            bricks -= diff; 
+            maxB.push(diff);
+            
+            
+            if(bricks < 0){
+                bricks += maxB.top(); 
+                maxB.pop(); 
+                ladders--; 
+            }
+
+            if(ladders < 0) break;
+        }
+        
+        return i;
     }
 };
