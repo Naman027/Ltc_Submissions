858716d0bf4c16df15ebb82bee3dbd35be4f44cd
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2022-06-28 20:46:56 +0530

    Time: 96 ms (48.28%), Space: 17.5 MB (21.80%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        int n = s.size();
-        set<int> st;
-        for(int i=1;i<=n;i++) st.insert(i);
-
-        map<char,int> mp;
-        for(int i=0;i<n;i++){
-            mp[s[i]]++;
+        int n=s.size();
+        int a[26];
+        for(int i=0;i<26;i++) a[i]=0;
+        for(int i=0;i<n;i++) a[s[i]-'a']++;
+        
+        vector<int> b;
+        for(int i=0;i<26;i++){
+            if(a[i]>=1) b.push_back(a[i]);
         }
-
-        map<int,int> mm;
-        for(auto i:mp){
-            if(st.count(i.second)) st.erase(i.second),mm[i.first-'a'] = 1;
-            else mm[i.first-'a'] = 0;
-        }
-
-        int ans = 0;
-        for(auto i:mm){
-            if(i.second==0){
-                int c = mp[i.first+'a'];
-                auto it = st.lower_bound(c);
-                if(it==st.begin()){
-                    ans+=c;
-                }
-                else{
-                    it--;
-                    ans+=(c- *it);
-                    st.erase(*it);
+        cout << b.size() << endl;
+        map<int,int> mp;
+        int ans=0;
+        sort(b.begin(),b.end());
+        mp[b[0]]++;
+        for(int i=1;i<b.size();i++){
+            if(b[i]==b[i-1]){
+                mp[b[i]]++;
+                for(int j=b[i]-1;j>=0;j--){
+                    if(mp[j]==0){
+                        ans+=(b[i]-j);
+                        if(j!=0) mp[j]++;
+                        break;
+                    }
                 }
             }
+            else{
+                mp[b[i]]++;
+            }
+            cout << ans << endl;
         }
-
         return ans;
-
     }
 };
